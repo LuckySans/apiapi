aea3a3b22a552a8cc5ba8ddc5fd4814d4991310d
LuckySans <ahmadrizky586@gmail.com>
2022-12-07 00:22:26 +0700

    up

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.use("cors")
+app.use(cors())
 
 // (7) import routes
 const mahasiswaRoutes = require('./routes/mahasiswa')
